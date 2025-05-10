4a133bd48591110f8c8a41a27733dd7eb4c20708
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-10 16:22:33 +0800

    Update init.lua
    added new keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,10 @@
------ Custom Commands
+----- Custom Keybinds
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('WQ', 'wq', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.keymap.set('n', 'a', 'I', { noremap = true })
+vim.keymap.set('n', 'I', 'a', { noremap = true })
 
 ----- Misc. configs
 -- Enable line numbers
@@ -12,8 +14,10 @@ vim.g.mapleader = ' '
 -- Example of setting an option
 vim.o.hlsearch = false
 -- Shows filename
-vim.o.statusline = "%t" 
--- Yanking to Clipboard 
+vim.o.statusline = "%F %m" 
+
+
+----- Yanking to Clipboard 
 vim.o.clipboard = "unnamedplus"
 vim.g.clipboard = {
     name = "wl-clipboard",
@@ -26,20 +30,24 @@ vim.g.clipboard = {
 -- Set the leader key to backslash (\)
 vim.g.mapleader = "\\"  -- This sets the leader key to backslash
 
--- Define a custom command to compile the current LaTeX file using pdflatex
+-- Define a custom command to compile the current LaTeX file using latexmk
 vim.api.nvim_create_user_command('CompileLatex', function()
   -- Save current file first
   vim.cmd('w')
 
   -- Get the current file's name
   local filename = vim.fn.expand('%')
+
+  -- Create the 'build' directory if it doesn't exist
+  vim.fn.system('mkdir -p build')
   
-  -- Run pdflatex to compile the LaTeX file
-  vim.cmd('!' .. 'pdflatex ' .. filename)
+  -- Run latexmk to compile the LaTeX file into PDF
+  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
 end, {})
 
--- Bind the custom command to a key (e.g., <leader>c)
-vim.api.nvim_set_keymap('n', '<leader>c', ':CompileLatex<CR>', { noremap = true, silent = true })
+-- Bind the custom command to a key (e.g., <leader>l)
+vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
+
 
 --------- COLORS
 vim.o.termguicolors = true  -- Enable true color support
@@ -49,3 +57,12 @@ vim.cmd('highlight NonText guibg=NONE')  -- Set non-text elements (e.g., trailin
 vim.cmd('highlight LineNr guibg=NONE')  -- Set line number background transparent
 vim.cmd('highlight StatusLine guibg=NONE')  -- Set status line background transparent
 vim.cmd('highlight TabLine guibg=NONE')  -- Set tab line background transparent
+
+---- packer test
+-- Initialize Packer
+require('packer').startup(function(use)
+  -- Example: Adding a plugin (e.g., nvim-treesitter)
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- Add other plugins here
+end)
