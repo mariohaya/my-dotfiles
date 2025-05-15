c8cbb8602ad95b18089bedf08dd7ff85f10640cf
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-15 12:54:08 +0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,82 +1,63 @@
------ Custom Keybinds
+----------- Initialize Packer -----------
+require('packer').startup(function(use)
+  use 'nvim-treesitter/nvim-treesitter'
+end)
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "lua", "python", "javascript", "latex"}, -- languages
+  highlight = { enable = true },
+  incremental_selection = { enable = true },
+  textobjects = { enable = true },
+}
+
+----------- Custom Keybinds -----------
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('WQ', 'wq', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.keymap.set('n', 'a', 'I', { noremap = true })
 vim.keymap.set('n', 'I', 'a', { noremap = true })
+vim.g.mapleader = ' ' -- Set the leader key to space
 
------ Misc. configs
--- Enable line numbers
-vim.wo.number = true
--- Set the leader key to space
-vim.g.mapleader = ' '
--- Example of setting an option
-vim.o.hlsearch = false
--- Shows filename
-vim.o.statusline = "%F %m" 
----------------------
+----------- Custom configs -----------
+vim.o.clipboard = "unnamedplus" -- Yanking to Clipboard (Wayland)
+vim.g.clipboard = {
+    name = "wl-clipboard",
+    copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
+    paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
+    cache_enabled = 1,}
+
+-- Yank directory \+d
 vim.keymap.set('n', '\\d', function()
   vim.fn.setreg('+', vim.fn.expand('%:p:h'))
   print('Directory copied to clipboard')
 end, { desc = 'Copy current file directory to clipboard' })
-
+-- Yank entire file \+y
 vim.keymap.set('n', '\\y', function()
   vim.cmd('normal! gg"+yG')
   print('Entire file copied to clipboard')
 end, { desc = 'Yank entire file to clipboard' })
 
-
------ Yanking to Clipboard 
-vim.o.clipboard = "unnamedplus"
-vim.g.clipboard = {
-    name = "wl-clipboard",
-    copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
-    paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
-    cache_enabled = 1,
-}
-
------ LaTeX compile
--- Set the leader key to backslash (\)
-vim.g.mapleader = "\\"  -- This sets the leader key to backslash
-
--- Define a custom command to compile the current LaTeX file using latexmk
+----------- LaTeX compiler -----------
+vim.g.mapleader = "\\" -- Set the leader key to backslash (\)
 vim.api.nvim_create_user_command('CompileLatex', function()
-  -- Save current file first
-  vim.cmd('w')
-
-  -- Get the current file's name
-  local filename = vim.fn.expand('%')
-
-  -- Create the 'build' directory if it doesn't exist
-  vim.fn.system('mkdir -p build')
-  
-  -- Run latexmk to compile the LaTeX file into PDF
-  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
+  vim.cmd('w')  -- Save current file first
+  local filename = vim.fn.expand('%')  -- Get the current file's name
+  vim.fn.system('mkdir -p build')  -- Create the 'build' directory 
+  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename) -- runs latexmk to compile
 end, {})
+vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true }) -- Bind the custom command to \+p
 
--- Bind the custom command to a key (e.g., <leader>l)
-vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
-
-
---------- COLORS
+----------- Visuals -----------
 vim.o.termguicolors = true  -- Enable true color support
+vim.wo.number = true -- Enable line numbers
+vim.o.statusline = "%F %m" -- Shows filename
+vim.cmd("colorscheme gruvbox") -- theme
 vim.cmd([[
-  highlight StatusLine guifg=#FFA500 guibg=#005f87
-  highlight StatusLineNC guifg=#FFA500 guibg=#303030]])
+  highlight StatusLine guifg=#FFA500 guibg=#005f87 
+  highlight StatusLineNC guifg=#FFA500 guibg=#303030]]) -- transparent bg
+ vim.cmd('highlight Normal guibg=NONE') -- normal text bg 
+ vim.cmd('highlight NonText guibg=NONE') -- non-text 
+ vim.cmd('highlight LineNr guibg=NONE') -- line no.
+ vim.cmd('highlight StatusLine guibg=NONE') -- status line bg
+ vim.cmd('highlight TabLine guibg=NONE') -- tab line bg
 
--- Make the background of Neovim transparent
-vim.cmd('highlight Normal guibg=NONE')  -- Set the background of normal text to transparent
-vim.cmd('highlight NonText guibg=NONE')  -- Set non-text elements (e.g., trailing spaces) transparent
-vim.cmd('highlight LineNr guibg=NONE')  -- Set line number background transparent
-vim.cmd('highlight StatusLine guibg=NONE')  -- Set status line background transparent
-vim.cmd('highlight TabLine guibg=NONE')  -- Set tab line background transparent
-
----- packer test
--- Initialize Packer
-require('packer').startup(function(use)
-  -- Example: Adding a plugin (e.g., nvim-treesitter)
-  use 'nvim-treesitter/nvim-treesitter'
-
-  -- Add other plugins here
-end)
