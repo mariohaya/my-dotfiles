18db2dacf1c6ec5b7112dc4b6ddcf45f7ddddd02
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-19 16:28:12 +0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,32 @@
+----------- Auto-install Packer -----------
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost init.lua source <afile> | PackerSync
+  augroup end
+]])
+
 ----------- Initialize Packer -----------
 require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'ellisonleao/gruvbox.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 ----------- Custom Keybinds -----------
@@ -10,45 +37,46 @@ vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.keymap.set('n', 'a', 'I', { noremap = true })
 vim.keymap.set('n', 'I', 'a', { noremap = true })
 
------------ Custom configs -----------
-vim.o.clipboard = "unnamedplus" -- Yanking to Clipboard (Wayland)
+----------- Clipboard Setup -----------
+vim.o.clipboard = "unnamedplus"
 vim.g.clipboard = {
-    name = "wl-clipboard", 
-    copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
-    paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
-    cache_enabled = 1,}
+  name = "wl-clipboard", 
+  copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
+  paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
+  cache_enabled = 1,
+}
 
--- Yank directory \+d
+----------- Yank Shortcuts -----------
 vim.keymap.set('n', '\\d', function()
   vim.fn.setreg('+', vim.fn.expand('%:p:h'))
   print('Directory copied to clipboard')
 end, { desc = 'Copy current file directory to clipboard' })
--- Yank entire file \+y
+
 vim.keymap.set('n', '\\y', function()
   vim.cmd('normal! gg"+yG')
   print('Entire file copied to clipboard')
 end, { desc = 'Yank entire file to clipboard' })
 
------------ LaTeX compiler -----------
-vim.g.mapleader = "\\" -- Set the leader key to backslash (\)
+----------- LaTeX Compiler -----------
+vim.g.mapleader = "\\"
 vim.api.nvim_create_user_command('CompileLatex', function()
-  vim.cmd('w')  -- Save current file first
-  local filename = vim.fn.expand('%')  -- Get the current file's name
-  vim.fn.system('mkdir -p build')  -- Create the 'build' directory 
-  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename) -- runs latexmk to compile
+  vim.cmd('w')
+  local filename = vim.fn.expand('%')
+  vim.fn.system('mkdir -p build')
+  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
 end, {})
-vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true }) -- Bind the custom command to \+p
 
------------ Visuals -----------
-vim.o.termguicolors = true  -- Enable true color support
-vim.wo.number = true -- Enable line numbers
-vim.o.statusline = "%F %m" -- Shows filename
-vim.cmd("colorscheme gruvbox") -- theme
-vim.cmd([[
-  highlight StatusLine guifg=#FFA500 guibg=#005f87 
-  highlight StatusLineNC guifg=#FFA500 guibg=#303030]]) -- transparent bg
- vim.cmd('highlight Normal guibg=NONE') -- normal text bg 
- vim.cmd('highlight NonText guibg=NONE') -- non-text 
- vim.cmd('highlight LineNr guibg=NONE') -- line no.
- vim.cmd('highlight StatusLine guibg=NONE') -- status line bg
- vim.cmd('highlight TabLine guibg=NONE') -- tab line bg
+vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
+
+----------- Visual Settings -----------
+vim.o.termguicolors = true
+vim.wo.number = true
+vim.o.statusline = "%F %m"
+
+vim.cmd("colorscheme gruvbox")
+
+vim.cmd('highlight Normal guibg=NONE') -- normal text bg 
+vim.cmd('highlight NonText guibg=NONE') -- non-text 
+vim.cmd('highlight LineNr guibg=NONE') -- line no.
+vim.cmd('highlight StatusLine guibg=NONE guifg=#FFA500') -- status line bg
+vim.cmd('highlight TabLine guibg=NONE') -- tab line bg
