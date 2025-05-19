bdc1564101e6d84c7ed8842a4bfe2e65902d65a0
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-19 15:00:05 +0800

    Update pre_suspend.sh

--- .config/hypr/scripts/pre_suspend.sh
+++ .config/hypr/scripts/pre_suspend.sh
@@ -3,13 +3,7 @@
 # Optional: log the event
 echo "[hypridle] Preparing for suspend at $(date)" >> ~/.hypridle.log
 
-# Optionally kill/restart Firefox
-# killall firefox  # or: pkill -f firefox
-
-# Optional: stop resource-heavy apps or services
-# systemctl --user stop firefox-wayland.service
-
-# Optional: give GPU time to settle
+# give GPU time to settle
 sleep 1
 
 exit 0
