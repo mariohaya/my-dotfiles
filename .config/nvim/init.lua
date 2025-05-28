3348b805eaa6e1ee890dcf0ee23558f643ebc774
mariohaya <152241805+mariohaya@users.noreply.github.com>
2025-05-28 14:48:37 +0800

    XeTex, spellcheck

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,7 @@ vim.cmd([[
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'ellisonleao/gruvbox.nvim'
+  use 'Vallen217/eidolon.nvim'
 
   if packer_bootstrap then
     require('packer').sync()
@@ -63,7 +64,7 @@ vim.api.nvim_create_user_command('CompileLatex', function()
   vim.cmd('w')
   local filename = vim.fn.expand('%')
   vim.fn.system('mkdir -p build')
-  vim.cmd('!' .. 'latexmk -pdf -outdir=build ' .. filename)
+  vim.cmd('!' .. 'latexmk -xelatex -outdir=build ' .. filename)
 end, {})
 
 vim.api.nvim_set_keymap('n', '<leader>l', ':CompileLatex<CR>', { noremap = true, silent = true })
@@ -73,10 +74,18 @@ vim.o.termguicolors = true
 vim.wo.number = true
 vim.o.statusline = "%F %m"
 
-vim.cmd("colorscheme gruvbox")
+
+-- vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme eidolon")
 
 vim.cmd('highlight Normal guibg=NONE') -- normal text bg 
 vim.cmd('highlight NonText guibg=NONE') -- non-text 
 vim.cmd('highlight LineNr guibg=NONE') -- line no.
 vim.cmd('highlight StatusLine guibg=NONE guifg=#FFA500') -- status line bg
 vim.cmd('highlight TabLine guibg=NONE') -- tab line bg
+vim.cmd('highlight Special guibg=NONE')
+vim.cmd('highlight Comment guibg=NONE')
+vim.cmd('highlight EndOfBuffer guibg=NONE')
+
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us' }  -- Set language; can add more like 'en_gb', 'es', etc.
